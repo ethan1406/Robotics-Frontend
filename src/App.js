5bf4fa018c3797ae66080ab386207d451ffe477f
Stanley <stanlejk@usc.edu>
2018-09-16 15:15:08 -0700

    Add bootstrap and created form page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './bootstrap/css/bootstrap.min.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React2</h1>
+          <h1 className="App-title">Video Data Pipeline</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form name="mainInputForm">
+            <div class="videoInputContainer">
+    				Video Data: <input name="videoFileInputBox" type="file" />
+                <br />
+                <label>
+                    OpenFace:
+                    <input name="OpenFaceCheckBox" type="checkbox" />
+                </label>
+                <br />
+                <label>
+                    OpenPose:
+                    <input name="OpenPoseCheckBox" type="checkbox" />
+                </label>
+            </div>
+            <br />
+            <div>
+                <label>
+    				OpenFace CSV Data:
+    				<input name="openFaceCSVFileInputBox" type="file" />
+    			</label>
+                <label>
+                    OpenPose CSV Data:
+                    <input name="openPoseCSVFileInputBox" type="file" />
+                </label>
+                <br />
+    			<input type="submit" value="Submit" />
+            </div>
+		</form>
       </div>
     );
   }
