953310b7f955ff9cd8cb6a79b34ef1da0727805f
Stanley <stanlejk@usc.edu>
2018-09-16 16:47:42 -0700

    Finish loading screen

--- src/Loading.js
+++ src/Loading.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import './bootstrap/css/bootstrap.min.css';
-import logo from './Preloader_4/Preloader_4.gif';
+import loadingGif from './Preloader_4/Preloader_4.gif';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="loading">
+        <img src={loadingGif} className="loadingSymbol" alt="loading" />
+        <p className="loadingMessage">Processing your data!</p>
+        <p className="loadingMessage">This might take a while<span>.</span><span>.</span><span>.</span></p>
       </div>
     );
   }
