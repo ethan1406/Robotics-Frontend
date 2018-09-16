f12b76a0c47d0f834bc182c540cca4a7ffa67f2a
Ethan Chang <ethanc@usc.edu>
2018-09-15 19:55:39 -0700

    to configure with django

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from './components/Main';
-import Navbar from './components/Navbar';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-//redux stuff
-
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-
-
-
-ReactDOM.render(
-	<BrowserRouter>
-		<div>
-			<Navbar />
-			<Switch>
-				<Route exact={true} path="/upload" component={Main}/>
-			</Switch>
-		</div>
-	</BrowserRouter>
-	,
-	document.getElementById('root')
-);
-
-
-
-
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
